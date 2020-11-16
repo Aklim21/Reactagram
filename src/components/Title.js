50aec5f4f494df5087274e4f78b989c22f96fc15
Aklim21 <anthonyjim101@gmail.com>
2020-11-16 01:08:25 +0000

    commit on master

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,7 +5,7 @@ const Title = () => {
     <div className="title">
       <h1>Reactagram</h1>
       <h2>Welcome to Reactagram</h2>
-      <p>definitely a unique idea and not related to that dodgy instagram website.</p>
+      <p>Definitely a unique idea and not related to that dodgy instagram website.</p>
     </div>
   )
 }
